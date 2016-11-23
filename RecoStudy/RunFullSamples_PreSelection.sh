4cacb72663dbf3cd97bfaae3fb44130cb623350d
mnaseri <mohsen.naseri@cern.ch>
2016-11-23 09:01:47 +0100

    add signal samples

--- RecoStudy/RunFullSamples_PreSelection.sh
+++ RecoStudy/RunFullSamples_PreSelection.sh
@@ -7,9 +7,7 @@
 ./CodexAnalyzer_Preselection.exe  OutFiles_PreSelection/WJetsToLNu.root ../ROOT80X/WJetsToLNu*.root
 ./CodexAnalyzer_Preselection.exe  OutFiles_PreSelection/VV.root ../ROOT80X/ZZ.root  ../ROOT80X/WW.root  ../ROOT80X/WZ.root
 ./CodexAnalyzer_Preselection.exe  OutFiles_PreSelection/SingleTop.root ../ROOT80X/ST_*.root
-
 ./CodexAnalyzer_Preselection.exe  OutFiles_PreSelection/Data.root ../ROOT80X/SingleMu.root
-
 ./CodexAnalyzer_Preselection.exe   OutFiles_PreSelection/Codex1000.root    ../ROOT80X/Codex_1000.root
 ./CodexAnalyzer_Preselection.exe   OutFiles_PreSelection/Codex1400.root    ../ROOT80X/Codex_1400.root
 ./CodexAnalyzer_Preselection.exe   OutFiles_PreSelection/Codex1800.root    ../ROOT80X/Codex_1800.root
